72dcdbf58fe38e912776a6a2ae57ec288b33e949
Yan Sym <yan.vsym1@gmail.com>
2025-06-04 14:45:13 -0300

    Update 📜 FAQ.py

--- src/pages/📜 FAQ.py
+++ src/pages/📜 FAQ.py
@@ -16,41 +16,9 @@ def faq_page():
             "É uma ferramenta de Chatbot conversacional que utiliza a técnica RAG (Retrieval-Augmented Generation) para responder perguntas de usuários com base nos documentos fornecidos.",
         ),
         (
-            "O que é RAG (Retrieval-Augmented Generation)?",
-            "RAG é uma técnica que combina a recuperação de informações (através de um banco de dados vetorial) com a geração de texto através de um LLMs (Large Language Models). Ele busca dados relevantes em uma base antes de gerar a resposta, garantindo maior precisão e contextualização na resposta do Chatbot.",
-        ),
-        (
-            "Qual a diferença entre um Chatbot tradicional e um Chatbot com RAG?",
-            "Um Chatbot tradicional responde apenas com base no modelo de linguagem (LLM) em que foi treinado, enquanto um com RAG busca informações em uma base externa antes de gerar a resposta.",
-        ),
-        (
             "Como faço para cadastrar uma nova aplicação?",
             "Na aba 'Aplicações', clique no botão 'Cadastrar Nova Aplicação', preencha os dados necessários, envie os documentos e depois clique em 'Salvar'. Sua aplicação será listada na página de Chatbots.",
-        ),
-        (
-            "Como posso desligar uma aplicação que não está mais em uso?",
-            "Caso queira desligar uma aplicação, comunique o time da QADS",
-        ),
-        (
-            "Como posso visualizar os Dashboards e métricas da ferramenta?",
-            "Vá até a aba 'Dashboards', onde você encontrará gráficos e estatísticas relevantes sobre o uso do Chatbot..",
-        ),
-        (
-            "O Chatbot consegue responder perguntas sobre qualquer assunto?",
-            "Não! Ele responde com base em conhecimentos gerais e nos documentos disponíveis no sistema. Se a resposta não for encontrada, o modelo pode não responder ou então gerar uma resposta pouco precisa.",
-        ),
-        (
-            "Os dados carregados são atualizados em tempo real?",
-            "Não! Para inserir novas informações, deve ser cadastrada uma nova aplicação.",
-        ),
-        (
-            "O chatbot pode ser integrado a outras plataformas?",
-            "No momento, não. Porém, caso haja necessidade, comunique o time da QADS para priorizar a atividade.",
-        ),
-        (
-            "Como faço para relatar um problema ou sugerir uma melhorias?",
-            "Mande um email para o time da QADS com a sua sugestão.",
-        ),
+        )
     ]
 
     # iterate question and answer
